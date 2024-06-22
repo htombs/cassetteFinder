9f8c68d33560114a4ee57349e0af6ddb7757cd5b
Harry Tombs <htombs91@gmail.com>
2024-06-22 22:56:26 +0100

    more adjustments trying to connect cassette_finder_db functions with eight_speed and distributor_table

--- Database/cassette_finder_db.py
+++ Database/cassette_finder_db.py
@@ -2,7 +2,6 @@ import sqlite3
 
 from tables import eight_speed
 from tables import distributor_table
-
 mydb = sqlite3.connect("cassette_finder.db")
 
 eight_speed.get_distributor_8spd(mydb,"Madison")
--- Database/tables/distributor_table.py
+++ Database/tables/distributor_table.py
@@ -3,16 +3,23 @@ import sqlite3
 connect = sqlite3.connect('cassette_finder.db')
 cursor = connect.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS distributor (distributor id INT PRIMARY KEY, distributor VARCHAR(255))''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS distributor_table (distributor_id INT PRIMARY KEY, distributor_name VARCHAR(255))''')
 
-cursor.execute('''INSERT INTO distributor (distributor_id, ditributor)
+cursor.execute('''INSERT INTO distributor_table (distributor_id, ditributor_name)
                VALUES
-               (1, Bob Elliot)
-               (2, Chicken Cyclekit)
-               (3, Greyville)
-               (4, Ison Distribution)
-               (5, Mackadams)
-               (6, Madison)
-               (7, Upgrade)
-               (8, ZyroFisher)
+               (1, "Bob Elliot"),
+               (2, "Chicken Cyclekit"),
+               (3, "Greyville"),
+               (4, "Ison Distribution"),
+               (5, "Mackadams"),
+               (6, "Madison"),
+               (7, "Upgrade"),
+               (8, "ZyroFisher")
                ''')
+
+connect.commit()
+
+# def get_table():
+#     cursor.execute('''SELECT * FROM db.sqlite_master WHERE type='table';''')
+
+# get_table()

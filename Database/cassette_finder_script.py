726bcbd707f8269bcd1dc9daff56fc77f5f82b87
Harry Tombs <h.tombs91@gmail.com>
2024-06-25 11:56:49 +0100

    created functions for table creations to run quicker

--- Database/cassette_finder_script.py
+++ Database/cassette_finder_script.py
@@ -1,6 +1,8 @@
 import sqlite3
 
-from tables.eight_speed import get_distributor_8spd
+from tables.eight_speed import get_distributor_8spd, insertEightSpeedData, createEightSpeedTable
 from tables import distributor_table
 
+# createEightSpeedTable()
+# insertEightSpeedData()
 get_distributor_8spd("Madison")

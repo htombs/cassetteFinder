aabeed75ffadde0305e1b4b98e11ff4157e595c3
Harry <h.tombs91@gmail.com>
2024-12-18 10:59:48 +0000

    table name changes with links to functions in script file

--- server/database/cassette_finder_script.py
+++ server/database/cassette_finder_script.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-from database.tables.eight_speed import insertEightSpeedData, createEightSpeedTable
+from database.tables.eight_speed import insertCassettesData, createCassettesTable
 # from database.tables.nine_speed import insertNineSpeedData, createNineSpeedTable
 # from database.tables.ten_speed import insertTenSpeedData, createTenSpeedTable
 # from database.tables.eleven_speed import insertElevenSpeedData, createElevenSpeedTable
 from database.tables import distributor_table
 
-createEightSpeedTable()
-insertEightSpeedData()
+insertCassettesData()
+createCassettesTable()
 
 # createNineSpeedTable()
 # insertNineSpeedData()

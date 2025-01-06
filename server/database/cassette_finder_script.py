0eda1e20852a6b8e1b35b8010c58ce48549a7128
Harry Tombs <h.tombs91@gmail.com>
2025-01-06 15:45:38 +0000

    cleaned up code and deleted unessary code from cassettes_finder_script.py

--- server/database/cassette_finder_script.py
+++ server/database/cassette_finder_script.py
@@ -1,21 +1,8 @@
 import sqlite3
 
 from tables.cassettes_table import createCassettesTable, insertCassettesData, dropCassTable
-# from database.tables.nine_speed import insertNineSpeedData, createNineSpeedTable
-# from database.tables.ten_speed import insertTenSpeedData, createTenSpeedTable
-# from database.tables.eleven_speed import insertElevenSpeedData, createElevenSpeedTable
-# from database.tables import distributor_table
 
 dropCassTable()
 
 createCassettesTable()
 insertCassettesData()
-
-# createNineSpeedTable()
-# insertNineSpeedData()
-# 
-# createTenSpeedTable()
-# insertTenSpeedData()
-# 
-# createElevenSpeedTable()
-# insertElevenSpeedData()

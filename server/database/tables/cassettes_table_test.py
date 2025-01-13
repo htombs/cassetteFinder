0bba86844e47e45882cc9b909c220dfd0990640b
Harry Tombs <h.tombs91@gmail.com>
2025-01-13 22:05:27 +0000

    continuing to write unit tests

--- server/database/tables/cassettes_table_test.py
+++ server/database/tables/cassettes_table_test.py
@@ -18,12 +18,31 @@ class TestCassettesTable(unittest.TestCase):
         sqlite3.connect.assert_called_with(WindowsPath(f"{Path.cwd()}/server/database/tables/cassette_finder.db"))
         self.assertEqual(dbc,'connection succeeded')
 
-
     def test_createCassettesTable(self):
         with patch('server.database.tables.cassettes_table.connect_database'):
-            sqlite3.connect = MagicMock(return_value=connect_test_database())
-            createCassettesTable()
-            sqlite3.connect.assert_called_with(WindowsPath(f"{Path.cwd()}/server/database/tables/cassette_finder_test.db"))
+            sqlite3.connect = MagicMock(return_value='connection succeeded')
+            dbc = connect_database()
+            dbc.assert_called_with('''CREATE TABLE IF NOT EXISTS cassettes_table
+               (id INTEGER PRIMARY KEY,
+               brand VARCHAR(255),
+               model VARCHAR(255),
+               partNumber VARCHAR(255),
+               speed INT,
+               ratio VARCHAR(10),
+               distributor VARCHAR(255),
+               rrp DECIMAL(10,2),
+               distributor_id INT,
+               FOREIGN KEY (distributor_id) REFERENCES distributor_table (distributor_id))''')
+            self.assertEqual()
+
+
+    # def test_createCassettesTable(self):
+    #     with patch('server.database.tables.cassettes_table.connect_database'):
+    #         sqlite3.connect = MagicMock(return_value=connect_test_database())
+    #         mock_cursor = sqlite3.connect.return_value.cursor.return_value
+    #         mock_cursor.execute(createCassettesTable())
+    #         sqlite3.connect.assert_called_with(WindowsPath(f"{Path.cwd()}/server/database/tables/cassette_finder_test.db"))
+    #         self.assertEqual()
 
 
 

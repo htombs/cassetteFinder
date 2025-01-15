56d9156f0efaf0c59f76d0f71bccde2808e9fbed
Harry Tombs <h.tombs91@gmail.com>
2025-01-15 17:34:00 +0000

    unit testing ongoing

--- server/database/tables/cassettes_table_test.py
+++ server/database/tables/cassettes_table_test.py
@@ -1,12 +1,12 @@
 from unittest.mock import MagicMock, Mock, patch
 import unittest
 import sqlite3
-from server.database.tables.cassettes_table import connect_database, createCassettesTable, insertCassettesData
+from server.database.tables.cassettes_table import connect_database, createCassettesTable, insertCassettesData, get_spd
 from pathlib import WindowsPath, Path
 
 @patch('server.database.tables.cassettes_table.connect_database')
 def connect_test_database(self):
-    return sqlite3.connect(WindowsPath(f"{Path.cwd()}/server/database/tables/cassette_finder_test.db"))
+    return sqlite3.connect(':memory:')
 
 class TestCassettesTable(unittest.TestCase):
 
@@ -20,35 +20,24 @@ class TestCassettesTable(unittest.TestCase):
 
     def test_createCassettesTable(self):
         with patch('server.database.tables.cassettes_table.connect_database'):
-            sqlite3.connect = MagicMock(return_value='connection succeeded')
-            dbc = connect_database()
-            dbc.assert_called_with('''CREATE TABLE IF NOT EXISTS cassettes_table
-               (id INTEGER PRIMARY KEY,
-               brand VARCHAR(255),
-               model VARCHAR(255),
-               partNumber VARCHAR(255),
-               speed INT,
-               ratio VARCHAR(10),
-               distributor VARCHAR(255),
-               rrp DECIMAL(10,2),
-               distributor_id INT,
-               FOREIGN KEY (distributor_id) REFERENCES distributor_table (distributor_id))''')
-            self.assertEqual()
+            conn = connect_test_database()
+            cursor = conn.cursor()
+            createCassettesTable()
+            sql = cursor.execute('''SELECT speed FROM cassettes_table''')
+            speed = []
+            conn.commit()
+            rows = sql.fetchall()
+            self.assertEqual(speed, rows, "it didn't work")
 
 
-    # def test_createCassettesTable(self):
-    #     with patch('server.database.tables.cassettes_table.connect_database'):
-    #         sqlite3.connect = MagicMock(return_value=connect_test_database())
-    #         mock_cursor = sqlite3.connect.return_value.cursor.return_value
-    #         mock_cursor.execute(createCassettesTable())
-    #         sqlite3.connect.assert_called_with(WindowsPath(f"{Path.cwd()}/server/database/tables/cassette_finder_test.db"))
-    #         self.assertEqual()
-
-
-
-    def test_insertCassettesData(self):
-        insertCassettesData()
-
+    def test_get_spd(self):
+        speed = "10"
+        ratio = "11-42"
+        brand = "shimano"
+        # input_list = [speed, ratio, brand]
+        got = get_spd(speed, ratio, brand)
+        want = list["10", "11-42", "shimano"]
+        self.assertEqual(got, want, "it didnt work")
 
 if __name__ == '__main__':
     unittest.main()

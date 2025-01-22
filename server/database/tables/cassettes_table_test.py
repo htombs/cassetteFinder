666482e3225f1e09fa8226d6a72c8e8fd37f6816
Harry Tombs <h.tombs91@gmail.com>
2025-01-22 14:07:41 +0000

    tests created for seed() and drop() functions

--- server/database/tables/cassettes_table_test.py
+++ server/database/tables/cassettes_table_test.py
@@ -19,6 +19,17 @@ class TestCassettesTable(unittest.TestCase):
         want = [] # In this case, we have no data, we just want to make sure there's no error
         self.assertEqual(got, want, f"test failed: got {got}, want {want}")
 
+    def test_seed(self):
+        test_database = Database(dbname=':memory:')
+
+        cassettes = CassettesTable(db=test_database)
+        cassettes.create()
+        cassettes.seed()
+
+        got = cassettes.select(f'''SELECT brand FROM {cassettes.table_name} WHERE brand="Shimano" LIMIT 1''',[])
+        want = [('Shimano',)]
+        self.assertEqual(got, want, f"test failed: got {got}, want {want}")
+
 
     def test_get_cassettes(self):
         test_database = Database(dbname=':memory:')
@@ -38,6 +49,21 @@ class TestCassettesTable(unittest.TestCase):
         got = cassettes.get_cassettes(speed=speed, ratio=ratio, brand=brand)
         want = [{'brand': 'Tifosi', 'model': '9X HG', 'part_number': 'TIF709A', 'speed': 9, 'ratio': '11-34', 'distributor': 'Chicken Cyclekit', 'rrp': 22.99, 'link': 'https://www.chickencyclekit.co.uk/'}] 
         self.assertEqual(got, want, f"test failed: got {got}, want {want}")
+
+    def test_drop(self):
+        with self.assertRaises(sqlite3.OperationalError):
+            test_database = Database(dbname=':memory:')
+    
+            cassettes = CassettesTable(db=test_database)
+            cassettes.create()
+            cassettes.seed()
+    
+            got = cassettes.select(f'''SELECT brand FROM {cassettes.table_name} WHERE brand="Shimano" LIMIT 1''',[])
+            want = [('Shimano',)]
+            self.assertEqual(got, want, f"test failed: got {got}, want {want}")
+    
+            cassettes.drop()
+            cassettes.select(f'''SELECT * FROM {cassettes.table_name}''',[])
 
 if __name__ == '__main__':
     unittest.main()

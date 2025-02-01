44b2499d0a211bbaee6e2e15baf01f66696d88c0
Harry Tombs <h.tombs91@gmail.com>
2025-02-01 21:27:02 +0000

    update to api_test.py trying unit test instead of BaseTestCase

--- server/database/tests/api_test.py
+++ server/database/tests/api_test.py
@@ -1,9 +1,13 @@
-from server.database.tests.test_base import BaseTestCase
-from server.database.tables.database import Database
+import unittest
 from server.api import app
+from server.database.tables.database import Database
 
+class FlaskintegrationTestCase(unittest.TestCase):
 
-class api_test(BaseTestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
+
     def test_api_route(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -24,3 +28,34 @@ class api_test(BaseTestCase):
         response = api('/__drop')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "Database dropped"})
+
+
+
+
+
+# from server.database.tests.test_base import BaseTestCase
+# from server.database.tables.database import Database
+# from server.api import app
+
+
+# class api_test(BaseTestCase):
+#     def test_api_route(self):
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.json, {"message": "Welcome to the Cassette Finder API"})
+
+#     def test_api_route_seed(self):
+#         test_database = Database(dbname=':memory:')
+#         api = app(db=test_database)
+
+#         response = api('/__seed')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.json, {"message": "Database seeded"})
+
+#     def test_api_route_drop(self):
+#         test_database = Database(dbname=':memory:')
+#         api = app(db=test_database)
+
+#         response = api('/__drop')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.json, {"message": "Database dropped"})

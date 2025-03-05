d252346c170c092d232a72e88ea784ad6c40f739
Harry Tombs <h.tombs91@gmail.com>
2025-03-05 17:00:46 +0000

    styles_mobile_css created with mobile screen size styling
    mobile table added to index.html
    
    data population functions added to script.js

--- client/js/script.js
+++ client/js/script.js
@@ -2,51 +2,52 @@
         // We'll add to this to make the various different API calls
 const apiurl = 'http://localhost:5000';
 
-        // Grab the output HTML element
+// Grab the output HTML element
 const output = document.getElementById('output');
 
-        // addResult simply takes an "item" JSON object
-        // creates a table row "tr" for that item element
-        // and adds it to the output element
+// addResult simply takes an "item" JSON object
+// creates a table row "tr" for that item element
+// and adds it to the output element
 function addResult(item) {
-        // if it's an object, just add the items to the output html.
+    // if it's an object, just add the items to the output html.
     const result = document.createElement('tr');
-        // NOTE: id's are prefixed with "pn_" just so it has a bit more context when reading them.
-        // result.id = `pn_${item.part_number}`;
-        // result.textContent = `${item.distributor} sells a ${item.brand} ${item.speed} speed ${item.ratio} for £${item.rrp}`;
-        // output.appendChild(result);
-        // this is outdated code that may come in handy in the future
+    result.classList.add('result');
+    // NOTE: id's are prefixed with "pn_" just so it has a bit more context when reading them.
+    // result.id = `pn_${item.part_number}`;
+    // result.textContent = `${item.distributor} sells a ${item.brand} ${item.speed} speed ${item.ratio} for £${item.rrp}`;
+    // output.appendChild(result);
+    // this is outdated code that may come in handy in the future
     const resultBr = document.createElement('td');
-        // this is creating the table data, living within the table row "tr"
+    // this is creating the table data, living within the table row "tr"
     resultBr.textContent = `${item.brand}`;
-        // showing the brand data from the database
+    // showing the brand data from the database
     result.appendChild(resultBr);
-         // adding the data to the row
-
+    // adding the data to the row
+    
     const resultCM = document.createElement('td');
     resultCM.textContent = `${item.model}`; // `` is used to define a string element, ${} is used for grabing js information
     result.appendChild(resultCM);
-
+    
     const resultSpeed = document.createElement('td');
     resultSpeed.textContent = `${item.speed}`;
     result.appendChild(resultSpeed);
-
+    
     const resultRatio = document.createElement('td');
     resultRatio.textContent = `${item.ratio}`;
     result.appendChild(resultRatio);
-
+    
     const resultPN = document.createElement('td');
     resultPN.textContent = `${item.part_number}`;
     result.appendChild(resultPN);
-
+    
     const resultPrice = document.createElement('td');
     resultPrice.textContent = `£${item.rrp}`; 
     result.appendChild(resultPrice);
-
+    
     const resultDistro = document.createElement('td');
     resultDistro.textContent = `${item.distributor}`;
     result.appendChild(resultDistro);
-
+    
     const resultLink = document.createElement('td');
     const link = document.createElement('a')
     link.href = item.link 
@@ -54,21 +55,71 @@ function addResult(item) {
     link.target = "_blank"; // this line opens the link in a new window
     resultLink.appendChild(link);
     result.appendChild(resultLink);
-
+    
     output.appendChild(result);
-        // adding the row to the table in the html file
+    // adding the row to the table in the html file
 }
 
-        // This event listener captures the "submit" event that happens when you submit a form
-        // You'll notice, we just grab the form by the tag selector, instead of by ID. 
-        // This is because there's only one form on the page.
-        // WARNING: This event will _also_ fire if another form on this page is submitted as well.
+function toggleMobileTable(ul) {
+    ul.classList.toggle('open');
+    }
+
+const mobile_output = document.getElementById('mobile_output');
+
+function addResultMobile(item) {
+    const result = document.createElement('li');
+    result.classList.add('result');
+
+    const resultBr = document.createElement('p');
+    resultBr.textContent = `${item.brand}`;
+    result.appendChild(resultBr);
+
+    const resultCM = document.createElement('p');
+    resultCM.textContent = `${item.model}`; 
+    result.appendChild(resultCM);
+
+    const resultSpeed = document.createElement('p');
+    resultSpeed.textContent = `${item.speed}`;
+    result.appendChild(resultSpeed);
+
+    const resultRatio = document.createElement('p');
+    resultRatio.textContent = `${item.ratio}`;
+    result.appendChild(resultRatio);
+
+    const resultPN = document.createElement('p');
+    resultPN.textContent = `${item.part_number}`;
+    result.appendChild(resultPN);
+
+    const resultPrice = document.createElement('p');
+    resultPrice.textContent = `£${item.rrp}`; 
+    result.appendChild(resultPrice);
+
+    const resultDistro = document.createElement('p');
+    resultDistro.textContent = `${item.distributor}`;
+    result.appendChild(resultDistro);
+
+    const resultLink = document.createElement('p');
+    const link = document.createElement('a')
+    link.href = item.link 
+    link.textContent = item.distributor;
+    link.target = "_blank"; // this line opens the link in a new window
+    resultLink.appendChild(link);
+    result.appendChild(resultLink);
+
+    mobile_output.appendChild(result);
+        // adding the row to the table in the html file
+    }
+
+// This event listener captures the "submit" event that happens when you submit a form
+// You'll notice, we just grab the form by the tag selector, instead of by ID. 
+// This is because there's only one form on the page.
+// WARNING: This event will _also_ fire if another form on this page is submitted as well.
 const form = document.querySelector('form').addEventListener('submit', (evt) => {
         // prevent the default form action taking place
         // This stops the page re-loading
     evt.preventDefault();
 
-        // fetch the selections fromt the Form data we submitted
+        // fetch the selections from the Form data we submitted
     const data = Object.fromEntries(new FormData(evt.target).entries());
 
         // clear the results on each submit
@@ -135,9 +186,11 @@ const form = document.querySelector('form').addEventListener('submit', (evt) => 
                 // Next we need to check if each item in the data object is an array [] or an object {}.
                 if (item.constructor == Array) {
                     item.forEach(itm => addResult(itm));
+                    item.forEach(itm => addResultMobile(itm));
                 }
 
                 if (item.constructor == Object) { addResult(item); }
+                if (item.constructor == Object) { addResultMobile(item); }
             });
 
         }).catch(error => {
